95ba27cca4315aa8870fdcd5113133e0078ba807
Abdullah Naser <abdnaser.exe@gmail.com>
2023-01-22 22:47:44 +0600

    Product, service, contact and about us page

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors');
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      ...colors,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
+        gray: colors.neutral,
+        primary: '#B61D1B',
+        low: '#17B978',
+        medium: '#0AA1DD',
+        high: '#FEB139',
+        critical: '#F55353'
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
