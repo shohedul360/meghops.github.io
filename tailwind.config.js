379bd4749c10acb07803b012986c84d49181aae6
shakil hossain <shakil@meghops.com>
2024-02-14 10:07:34 +0600

    add more sectoin in vapt page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./**/*.html"],
   theme: {
     colors: {
       ...colors,
