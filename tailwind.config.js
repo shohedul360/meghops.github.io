a83dc5d101f8bbdf79fd741fa57280b2731abbf3
shakil hossain <shakil@meghops.com>
2024-03-20 14:31:35 +0600

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,7 @@ module.exports = {
   },
   darkMode: 'class',
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 }
